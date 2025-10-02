66d82644e5127817b5a5f28915fa8c819272f056
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-10-02 15:50:02 +0530

    setup fake api and useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import MainComponent from "./MainComponent";
 
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducerFn(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducerFn, initialState);
+
+  useEffect(function () {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+
+        if (!res.ok) throw new Error("Something went wrong with fetching data");
+
+        const data = await res.json();
+        //dispatching action of type `dataReceived'
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        //dispatching action of type `dataFailed`
+        dispatch({ type: "dataFailed" });
+        console.error(err.message);
+      }
+    }
+
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <Header />

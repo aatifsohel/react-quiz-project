902180ec75b0591416ae2019764e9261bac29dce
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-10-26 13:42:25 +0530

    add src-no-context directory

---
+++ src-no-context/components/App.jsx
@@ -0,0 +1,187 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import MainComponent from "./MainComponent";
+import Loader from "./Loader";
+import Error from "./ErrorComponent";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SEC_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+  // it's here bcoz we need to re-render after update index
+  index: 0,
+  // initial answer will be null
+  answer: null,
+  totalPoints: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+function reducerFn(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SEC_PER_QUESTION,
+      };
+
+    case "newAnswer": {
+      // current question based on index from questions arr
+      // each question has diffeerent point & total points is in state
+      // we can also use this logic where we handle the event but that will be against the useReducer hook rule
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        totalPoints:
+          action.payload === question.correctOption
+            ? state.totalPoints + question.points
+            : state.totalPoints,
+      };
+    }
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.totalPoints > state.highscore
+            ? state.totalPoints
+            : state.highscore,
+      };
+
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+        highscore: state.highscore,
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [
+    {
+      questions,
+      status,
+      index,
+      answer,
+      totalPoints,
+      highscore,
+      secondsRemaining,
+    },
+    dispatch,
+  ] = useReducer(reducerFn, initialState);
+
+  const numQuestions = questions.length;
+
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+  useEffect(function () {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+
+        if (!res.ok) throw new Error("Something went wrong with fetching data");
+
+        const data = await res.json();
+        //dispatching action of type `dataReceived'
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        //dispatching action of type `dataFailed`
+        dispatch({ type: "dataFailed" });
+        console.error(err.message);
+      }
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <MainComponent>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              totalPoints={totalPoints}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                numQuestions={numQuestions}
+                index={index}
+                answer={answer}
+              />
+            </Footer>
+          </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            totalPoints={totalPoints}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </MainComponent>
+    </div>
+  );
+}
